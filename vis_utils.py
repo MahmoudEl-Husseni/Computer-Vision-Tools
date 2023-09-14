d6d780705722ca13472da5abb8b53f8b4b0d8cf7
MahmoudEl-Husseni <oudm753@gmail.com>
2023-09-14 22:41:18 +0300

    Visualization utils updated

--- vis_utils.py
+++ vis_utils.py
@@ -3,26 +3,93 @@
 # ===================================================================================================================== #
 # Plot images: 
 def plot_images(images, titles=None, rows=-1, cols=-1):
-  if titles is None:
-    titles = np.arange(len(images))
 
-  n_images = len(images) 
-  if rows==-1 and cols==-1:
-    rows = np.sqrt(n_images).__floor__()
-    cols = (n_images / rows).__ceil__()
+  """
+  Plots a given set of images.
+
+  Args:
+    images (list): A list of images.
+    titles (list, optional): A list of titles for the images. If not provided,
+        the titles will be the indices of the images.
+    rows (int, optional): The number of rows in the plot. Defaults to -1,
+        which means that the number of rows will be determined automatically.
+    cols (int, optional): The number of columns in the plot. Defaults to -1,
+        which means that the number of columns will be determined automatically.
+
+  Returns:
+    None.
+
+  Raises:
+    TypeError: If images is not a list of images.
+    TypeError: If titles is not None and is not a list of strings.
+    Exception: If there is an error plotting an image.
+  """
+
+  # Check the input arguments.
+  if not isinstance(images, list):
+    raise TypeError('images must be a list of images')
+  if titles is not None and not isinstance(titles, list):
+    raise TypeError('titles must be a list of strings')
+  if titles is None: 
+    titles = np.arange(len(images))
+  # Set the default values for rows and cols if not specified.
+  if rows == -1 and cols == -1:
+    rows = np.floor(np.sqrt(len(images))).astype(int)
+    cols = np.ceil((len(images) / rows)).astype(int)
+
+  # Create the figure and subplots.
   fig, axs = plt.subplots(rows, cols, figsize=(20, 12))
   if axs.ndim==1: axs = np.expand_dims(axs, 0)
+
+  # Plot the images.
   for row in range(rows):
     for col in range(cols):
-      i = row*cols + col
-      if(i>=len(images)): break
-      img = images[i]
-      ax = axs[row][col]
-      ax.imshow(img)
-      ax.set_title(f'{titles[i]}')
-      ax.set_xticks([])
-      ax.set_yticks([])
+        i = row*cols + col
+        if(i>=len(images)): break
+        img = images[i]
+        ax = axs[row][col]
+        try:
+            ax.imshow(img)
+            ax.set_title(f'{titles[i]}')
+            ax.set_xticks([])
+            ax.set_yticks([])
+        except:
+            raise Exception('Error plotting image')
+
+  # Show the plot.
   plt.show()
+# ============================================================  
+# Plots a random selection of images from the given directory.  
+def plot_random_images_from_dir(path, n_images=64):
+
+    """
+    Plots a random selection of `n_images` images from the directory `path`.
+
+    Args:
+        path (str): The path to the directory containing the images.
+        n_images (int, optional): The number of images to plot. Defaults to 64.
+
+    Returns:
+        None.
+    """
+
+    # Get the list of all images in the directory.
+    IMAGES = os.listdir(path)
+
+    # Generate a random sample of `n_images` indices.
+    indices = np.random.randint(0, len(IMAGES), n_images)
+
+    # Load the images at the specified indices.
+    def load(im_path):
+        """Loads an image from the given path."""
+        img = cv.imread(os.path.join(path, im_path))
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        return img
+
+    images = [load(IMAGES[i]) for i in indices]
+
+    # Plot the images.
+    plot_images(images)
 # ============================================================  
 # PLot Histogram of colors
 def plot_chist(im):
